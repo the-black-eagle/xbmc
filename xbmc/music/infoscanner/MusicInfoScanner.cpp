4cfee5c8fa9cb07f1e54b7f0390f5e835052e467
the-black-eagle <g.moore@gmx.co.uk>
2022-02-02 21:24:47 +0000

    [MUSIC] Use taglib to scan m4a/m4b audiobook tags

--- xbmc/music/infoscanner/MusicInfoScanner.cpp
+++ xbmc/music/infoscanner/MusicInfoScanner.cpp
@@ -583,11 +583,31 @@ CInfoScanner::INFO_RET CMusicInfoScanner::ScanTags(const CFileItemList& items,
     m_currentItem++;
 
     CMusicInfoTag& tag = *pItem->GetMusicInfoTag();
+    std::string orig_title = "";
+    int iDuration = 0;
+    if (pItem->IsAudioBook())
+    { // save these values from CAudioBookFileDirectory.cpp and overwrite the tag from taglib with
+      // them. SetLoaded to false so we call taglib otherwise we only get a very basic set of tags
+      // CAudioBookFileDirectory.cpp needs to set SetLoaded to true otherwise browsing via files
+      // does not work correctly.
+      orig_title = pItem->GetMusicInfoTag()->GetTitle();
+      iDuration = pItem->GetMusicInfoTag()->GetDuration();
+      pItem->GetMusicInfoTag()->SetLoaded(false);
+    }
     if (!tag.Loaded())
     {
       std::unique_ptr<IMusicInfoTagLoader> pLoader (CMusicInfoTagLoaderFactory::CreateLoader(*pItem));
       if (nullptr != pLoader)
         pLoader->Load(pItem->GetPath(), tag);
+      if(pItem->IsAudioBook())
+      {
+        if (!orig_title.empty())
+          pItem->GetMusicInfoTag()->SetTitle(orig_title);// Chapter name from original tag
+        pItem->GetMusicInfoTag()->SetDuration(iDuration);// duration from original tag
+        // fixup the correct track number (one track, with multiple chapters split into tracks)
+        pItem->GetMusicInfoTag()->SetTrackNumber(i + 1);
+        pItem->GetMusicInfoTag()->SetLoaded(true);
+      }
     }
 
     if (m_handle && m_itemCount>0)
