b34539197281dd985f69cd9c7704067b36cb9c88
the-black-eagle <18698554+the-black-eagle@users.noreply.github.com>
2025-03-08 16:14:18 +0000

    Update MusicDatabase to add properties to item

--- xbmc/music/MusicDatabase.cpp
+++ xbmc/music/MusicDatabase.cpp
@@ -9419,7 +9419,7 @@ void CMusicDatabase::UpdateTables(int version)
   // The last schema change needing forced rescanning was 73.
   // This is because Kodi can now read and process extra tags involved in the creation of box sets
 
-  SetMusicNeedsTagScan(73);
+  SetMusicNeedsTagScan(83);
 
   // After all updates, store the original db version.
   // This indicates the version of tag processing that was used to populate db
@@ -12814,6 +12814,13 @@ void CMusicDatabase::SetPropertiesFromAlbum(CFileItem& item, const CAlbum& album
   item.SetProperty("album_releasetype", CAlbum::ReleaseTypeToString(album.releaseType));
   item.SetProperty("album_duration",
                    StringUtils::SecondsToTimeString(album.iAlbumDuration, TIME_FORMAT_GUESS));
+
+  if (album.songs.size() >0)
+  {
+    item.SetProperty("album_codec", album.songs[0].strCodec);
+    item.SetProperty("album_bitspersample", album.songs[0].iBitsPerSample);
+    item.SetProperty("album_samplerate", album.songs[0].iSampleRate);
+  }
 }
 
 void CMusicDatabase::SetPropertiesForFileItem(CFileItem& item)
@@ -12844,7 +12851,7 @@ void CMusicDatabase::SetPropertiesForFileItem(CFileItem& item)
   if (idAlbum > -1)
   {
     CAlbum album;
-    if (GetAlbum(idAlbum, album, false))
+    if (GetAlbum(idAlbum, album, true))
       SetPropertiesFromAlbum(item, album);
   }
 }
