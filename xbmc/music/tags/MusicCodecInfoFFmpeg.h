b371ec7ed553bbbd03fda1f234b00063f96a65e5
the-black-eagle <18698554+the-black-eagle@users.noreply.github.com>
2025-03-08 16:17:04 +0000

    [MUSIC] Make sure we have a valid audio track to extract info from

--- xbmc/music/tags/MusicCodecInfoFFmpeg.h
+++ xbmc/music/tags/MusicCodecInfoFFmpeg.h
@@ -5,22 +5,25 @@
  *  SPDX-License-Identifier: GPL-2.0-or-later
  *  See LICENSES/README.md for more information.
  */
+ 
+#pragma once
 
 #include <string>
 
-typedef struct musicCodecInfo
+struct musicCodecInfo
 {
+public:
   int bitsPerSample = 0;
   int sampleRate = 0;
   int bitRate = 0;
   int channels = 0;
   int duration = 0;
   std::string codecName;
-}
-fileInfo;
+};
+
 
 class CMusicCodecInfoFFmpeg
 {
 public:
-  bool GetMusicCodecInfo(const std::string& strFileName, musicCodecInfo& codec_info);
+  static bool GetMusicCodecInfo(const std::string& strFileName, musicCodecInfo& codec_info);
 };
