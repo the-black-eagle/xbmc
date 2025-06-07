9ed407b37414bad412900f03f343aa1bf3250635
the-black-eagle <18698554+the-black-eagle@users.noreply.github.com>
2025-06-07 13:17:58 +0100

    [MUSIC] Only require music source(s) for mkv or mp4 files, not mka etc

--- xbmc/filesystem/FileDirectoryFactory.cpp
+++ xbmc/filesystem/FileDirectoryFactory.cpp
@@ -237,21 +237,26 @@ IFileDirectory* CFileDirectoryFactory::Create(const CURL& url, CFileItem* pItem,
     return NULL;
   }
 
-  if (pItem->IsAudioBook() || pItem->IsMatroskaAudio() || pItem->IsMatroskaVideo() ||
-      (url.IsFileType("mp4")))
+  if (pItem->IsAudioBook() || pItem->IsMatroskaAudio())
   {
     if (!pItem->HasMusicInfoTag() || pItem->GetEndOffset() <= 0)
     {
-      VECSOURCES* musicSources = CMediaSourceSettings::GetInstance().GetSources("music");
-      bool isSource;
-      int sourceIndex = CUtil::GetMatchingSource(pItem->GetPath(), *musicSources, isSource);
-      if (sourceIndex >= 0 && sourceIndex < static_cast<int>(musicSources->size()))
-      {
-        std::unique_ptr<CAudioBookFileDirectory> pDir(new CAudioBookFileDirectory);
+      std::unique_ptr<CAudioBookFileDirectory> pDir(new CAudioBookFileDirectory);
         if (pDir->ContainsFiles(url))
           return pDir.release();
-      }
-      return NULL;
+    }
+    return NULL;
+  }
+  else if ( pItem->IsMatroskaVideo() || url.IsFileType("mp4"))
+  {
+    VECSOURCES* musicSources = CMediaSourceSettings::GetInstance().GetSources("music");
+    bool isSource;
+    int sourceIndex = CUtil::GetMatchingSource(pItem->GetPath(), *musicSources, isSource);
+    if (sourceIndex >= 0 && sourceIndex < static_cast<int>(musicSources->size()))
+    {
+      std::unique_ptr<CAudioBookFileDirectory> pDir(new CAudioBookFileDirectory);
+      if (pDir->ContainsFiles(url))
+        return pDir.release();
     }
     return NULL;
   }
