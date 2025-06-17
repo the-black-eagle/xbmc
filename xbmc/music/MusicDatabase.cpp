2b00dafeb32bd63168e67d67a6c37df05084b102
the-black-eagle <18698554+the-black-eagle@users.noreply.github.com>
2025-06-17 22:06:14 +0100

    [MUSIC]  Fix type, missing curly bracket

--- xbmc/music/MusicDatabase.cpp
+++ xbmc/music/MusicDatabase.cpp
@@ -14049,6 +14049,7 @@ std::vector<std::string> CMusicDatabase::GetUsedImages(
     CLog::LogF(LOGERROR, "failed");
   }
   return {};
+}
 void CMusicDatabase::GetMusicDetails(CFileItemList& items, std::string& reqField)
 {
   try
