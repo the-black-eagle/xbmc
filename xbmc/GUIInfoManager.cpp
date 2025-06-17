6bc1984de84eb3bd4c77c6d6941e197c20575ec3
the-black-eagle <18698554+the-black-eagle@users.noreply.github.com>
2025-06-17 22:03:00 +0100

    [MUSIC] Update gui arrays for Piers

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -835,7 +835,7 @@ constexpr std::array<InfoMap, 7> integer_bools = {{
 ///     <p>
 ///   }
 // clang-format off
-constexpr std::array<InfoMap, 57> player_labels = {{
+constexpr std::array<InfoMap, 59> player_labels = {{
     {"hasmedia",              PLAYER_HAS_MEDIA},
     {"hasaudio",              PLAYER_HAS_AUDIO},
     {"hasvideo",              PLAYER_HAS_VIDEO},
@@ -893,8 +893,8 @@ constexpr std::array<InfoMap, 57> player_labels = {{
     {"scenemarkers",          PLAYER_SCENE_MARKERS},
     {"hasscenemarkers",       PLAYER_HAS_SCENE_MARKERS},
     {"chapters",              PLAYER_CHAPTERS},
-    {"chapterlength",PLAYER_CHAPTERLENGTH},
-    {"chapterelapsed",PLAYER_CHAPTER_ELAPSED},
+    {"chapterlength",         PLAYER_CHAPTERLENGTH},
+    {"chapterelapsed",        PLAYER_CHAPTER_ELAPSED},
 }};
 // clang-format on
 /// \page modules__infolabels_boolean_conditions
@@ -4117,7 +4117,7 @@ constexpr std::array<InfoMap, 46> musicplayer = {{
 ///
 /// -----------------------------------------------------------------------------
 // clang-format off
-constexpr std::array<InfoMap, 82> videoplayer = {{
+constexpr std::array<InfoMap, 85> videoplayer = {{
     {"title",                 VIDEOPLAYER_TITLE},
     {"genre",                 VIDEOPLAYER_GENRE},
     {"country",               VIDEOPLAYER_COUNTRY},
@@ -4200,9 +4200,9 @@ constexpr std::array<InfoMap, 82> videoplayer = {{
     {"episodepart",           VIDEOPLAYER_EPISODEPART},
     {"mediaproviders",        VIDEOPLAYER_MEDIAPROVIDERS},
     {"titleextrainfo",        VIDEOPLAYER_TITLE_EXTRAINFO},
-    { "chapterlength",    VIDEOPLAYER_CHAPTERLENGTH},
-    { "chapterelapsed",   PLAYER_CHAPTER_ELAPSED},
-    { "ismusicvideo",     VIDEOPLAYER_IS_MUSIC_VIDEO},
+    {"chapterlength",    VIDEOPLAYER_CHAPTERLENGTH},
+    {"chapterelapsed",   PLAYER_CHAPTER_ELAPSED},
+    {"ismusicvideo",     VIDEOPLAYER_IS_MUSIC_VIDEO},
 }};
 
 // clang-format on
@@ -7355,7 +7355,7 @@ constexpr std::array<InfoMap, 3> container_str = {{
 /// -----------------------------------------------------------------------------
 // clang-format off
 
-constexpr std::array<InfoMap, 227> listitem_labels = {{ 
+constexpr std::array<InfoMap, 229> listitem_labels = {{ 
                                   {"thumb",            LISTITEM_THUMB },
                                   { "icon",             LISTITEM_ICON },
                                   { "actualicon",       LISTITEM_ACTUAL_ICON },
