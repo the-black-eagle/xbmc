839449e029515e845dd5461b8a43a793be09cd3b
the-black-eagle <18698554+the-black-eagle@users.noreply.github.com>
2025-06-18 07:35:49 +0100

    [GUIInfoManager] Avoid a nullptr crash

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -11691,6 +11691,8 @@ int CGUIInfoManager::TranslateListItem(const Property& cat,
   {
     for (const auto& listitem_label : listitem_labels) // these ones don't have or need an id
     {
+      if (nullptr == listitem_label.str)
+        continue;
       if (prop.Name() == listitem_label.str)
       {
         ret = listitem_label.val;
